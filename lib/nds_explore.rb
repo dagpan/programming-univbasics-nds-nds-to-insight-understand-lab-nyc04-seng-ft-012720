e2d0b55f068b03284e18e7a1ff67780254838d9c
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-19 17:28:42 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -20,12 +20,12 @@ def print_first_directors_movie_titles
   directors_database
   row = 0
   while row < nds.length do
-      column = 0 
-      while column < nds[row].length do
-         nds[0] = director
+     column = 0 
+     while column < nds[row].length do
+        nds[0] = director
 #         puts "#{nds[0]"
-         puts director
-         column += 1
+        puts director
+        column += 1
       end
       row += 1
   end
