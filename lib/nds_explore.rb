76885fa3afdd4a166c57e4c280c56ba75a2e17a6
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-19 17:24:15 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,7 +16,7 @@ def pretty_print_nds(nds)
 end
 
 directors_database
-def print_first_directors_movie_titles(nds)
+def print_first_directors_movie_titles
   row = 0
   while row < nds.length do
       column = 0 
